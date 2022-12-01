5ead10c2e47fd899932f5608d196a2c15e259c8f
leungpo <polly.leung@oocl.com>
2022-12-01 14:41:45 +0800

    feat: return Fuzz when order is multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,12 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static final String FUZZ = "Fuzz";
+
     public String countOff(int order) {
+        if(order % 3 == 0){
+            return FUZZ;
+        }
         return Integer.toString(order);
     }
 }
